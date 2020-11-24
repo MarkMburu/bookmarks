a522315724caeadb964e0d19ce12e7667a7dbed9
MarkMburu <markmburu685@gmail.com>
2020-11-24 15:17:07 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require("mongoose")
+const middlewares = require("./middleware/middleware")
+const morgan = require('morgan');
+const helmet = require('helmet');
+
 require('dotenv').config();
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(morgan('common'));
+app.use(helmet());
 
 const uri = process.env.MONGO_URI
 
@@ -22,6 +28,9 @@ db.on('error',(err)=>{
 
 const bookmarkRouter = require('./routes/bookmarks-route.js');
 app.use('/bookmarks',bookmarkRouter);
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port ,()=>{
